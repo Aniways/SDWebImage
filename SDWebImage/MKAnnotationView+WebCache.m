Aniways/SDWebImage

commit 6406d8e526c3c19878a490aac3056dad5b5faf74

Modifies SDWebImagePrefetcher prefetching logic. - Removes recursive calls to startPrefetchingAtIndex. - Uses dispatch_apply with striding to optimize the performance.

SDWebImage/SDWebImagePrefetcher.m
@@ -78,12 +78,7 @@
                             didPrefetchURL:self.prefetchURLs[index]
                              finishedCount:self.finishedCount
                                 totalCount:self.prefetchURLs.count
-            ];
-        }
-        if (self.prefetchURLs.count > self.requestedCount) {
-            dispatch_async(self.prefetcherQueue, ^{
-                [self startPrefetchingAtIndex:self.requestedCount];
-            });
+             ];
         }
         else if (self.finishedCount == self.requestedCount) {
             [self reportStatus];
@@ -102,7 +97,7 @@
         [self.delegate imagePrefetcher:self
                didFinishWithTotalCount:(total - self.skippedCount)
                           skippedCount:self.skippedCount
-        ];
+         ];
     }
 }
 
@@ -117,14 +112,29 @@
     self.completionBlock = completionBlock;
     self.progressBlock = progressBlock;
 
-    if(urls.count == 0){ 
+    __weak SDWebImagePrefetcher *weakSelf = self;
+
+    if(urls.count == 0){
         if(completionBlock){
             completionBlock(0,0);
         }
     }else{
-        // Starts prefetching from the very first image on the list with the max allowed concurrency
-        NSUInteger listCount = self.prefetchURLs.count;
-        for (NSUInteger i = 0; i < self.maxConcurrentDownloads && self.requestedCount < listCount; i++) {
+        // http://oleb.net/blog/2013/07/parallelize-for-loops-gcd-dispatch_apply/
+        // Optimize the maxConcurrentdownloads for effeciency. Since caching operations are involved that are non-trivial using
+        // dispatch_apply might be helpful.
+
+        NSInteger maxNumberOfImages = self.prefetchURLs.count;
+
+        dispatch_apply(maxNumberOfImages/self.maxConcurrentDownloads, dispatch_get_global_queue(self.prefetcherQueue, 0), ^(size_t index) {
+            size_t i = index * self.maxConcurrentDownloads;
+            size_t stop = i + self.maxConcurrentDownloads;
+            do{
+                [weakSelf startPrefetchingAtIndex:i++];
+            }while (i < stop);
+        });
+
+        // Download remaining images.
+        for (size_t i = maxNumberOfImages - (maxNumberOfImages % self.maxConcurrentDownloads); i < (size_t)maxNumberOfImages; i++) {
             [self startPrefetchingAtIndex:i];
         }
     }

commit 0a9094a1b6c2ba28ca2fe807ea565c005a757bb5

added all aniways required changes such as - return image from disk synchronously, expose imageData * mergeFromOrigin: (152 commits)   Fix compilation error introduced in commit 6406d8e526c3c19878a490aac3056dad5b5faf74   Updated Readme - fixed type   Updated .gitignore   Making sure all access to self.runningOperations is synchronized   Code style   Modifies SDWebImagePrefetcher prefetching logic. - Removes recursive calls to startPrefetchingAtIndex. - Uses dispatch_apply with striding to optimize the performance.   Replaced #1258 - Removing "and" from method names per https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/CodingGuidelines/Articles/NamingMethods.html   Xcode 7.1 updates   Replaced #1345 - Added urlCredential property to SDWebImageDownloader   Updated Readme   Update project settings for Xcode 7   Add tvOS Framework target   iOS 9 App Transport Security Demo Target   Fixed the error that was causing the Travis builds to fail   Added CHANGELOG.md   spelling   Memory usage upgrade   Updated Readme (subspecs)   Updated #976 according to comments via review   don't to perform download if url is "" but not nil   ...
Conflicts:
	SDWebImage/MKAnnotationView+WebCache.m
	SDWebImage/SDImageCache.m
	SDWebImage/UIButton+WebCache.m
	SDWebImage/UIImageView+HighlightedWebCache.m
	SDWebImage/UIImageView+WebCache.m

SDWebImage/MKAnnotationView+WebCache.m
@@ -46,7 +46,7 @@ static char imageURLKey;
 
     if (url) {
         __weak __typeof(self)wself = self;
-        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:nil completed:^(UIImage *image, NSData *imageData, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
             if (!wself) return;
             dispatch_main_sync_safe(^{
                 __strong MKAnnotationView *sself = wself;

SDWebImage/SDImageCache.h
@@ -24,7 +24,7 @@ typedef NS_ENUM(NSInteger, SDImageCacheType) {
     SDImageCacheTypeMemory
 };
 
-typedef void(^SDWebImageQueryCompletedBlock)(UIImage *image, SDImageCacheType cacheType);
+typedef void(^SDWebImageQueryCompletedBlock)(UIImage *image, NSData *imageData, SDImageCacheType cacheType);
 
 typedef void(^SDWebImageCheckCacheCompletionBlock)(BOOL isInCache);
 
@@ -146,14 +146,14 @@ typedef void(^SDWebImageCalculateSizeBlock)(NSUInteger fileCount, NSUInteger tot
  *
  * @param key The unique key used to store the wanted image
  */
-- (UIImage *)imageFromMemoryCacheForKey:(NSString *)key;
+- (void)imageFromMemoryCacheForKey:(NSString *)key completion:(void(^)(UIImage * image, NSData *imageData))completion;
 
 /**
  * Query the disk cache synchronously after checking the memory cache.
  *
  * @param key The unique key used to store the wanted image
  */
-- (UIImage *)imageFromDiskCacheForKey:(NSString *)key;
+- (void)imageFromDiskCacheForKey:(NSString *)key completion:(void(^)(UIImage * image, NSData *imageData))completion;
 
 /**
  * Remove the image from memory and disk cache synchronously

SDWebImage/SDImageCache.m
@@ -203,7 +203,7 @@ FOUNDATION_STATIC_INLINE NSUInteger SDCacheCostForImage(UIImage *image) {
     // if memory cache is enabled
     if (self.shouldCacheImagesInMemory) {
         NSUInteger cost = SDCacheCostForImage(image);
-        [self.memCache setObject:image forKey:key cost:cost];
+        [self.memCache setObject:imageData forKey:key cost:cost];
     }
 
     if (toDisk) {
@@ -263,11 +263,11 @@ FOUNDATION_STATIC_INLINE NSUInteger SDCacheCostForImage(UIImage *image) {
 }
 
 - (void)storeImage:(UIImage *)image forKey:(NSString *)key {
-    [self storeImage:image recalculateFromImage:YES imageData:nil forKey:key toDisk:YES];
+    [self storeImage:image recalculateFromImage:YES imageData:UIImagePNGRepresentation(image) forKey:key toDisk:YES];
 }
 
 - (void)storeImage:(UIImage *)image forKey:(NSString *)key toDisk:(BOOL)toDisk {
-    [self storeImage:image recalculateFromImage:YES imageData:nil forKey:key toDisk:toDisk];
+    [self storeImage:image recalculateFromImage:YES imageData:UIImagePNGRepresentation(image) forKey:key toDisk:toDisk];
 }
 
 - (BOOL)diskImageExistsWithKey:(NSString *)key {
@@ -291,26 +291,30 @@ FOUNDATION_STATIC_INLINE NSUInteger SDCacheCostForImage(UIImage *image) {
     });
 }
 
-- (UIImage *)imageFromMemoryCacheForKey:(NSString *)key {
-    return [self.memCache objectForKey:key];
+- (void)imageFromMemoryCacheForKey:(NSString *)key completion:(void(^)(UIImage * image, NSData *imageData))completion{
+    NSData *imageData = [self.memCache objectForKey:key];
+    completion([UIImage imageWithData:imageData], imageData);
 }
 
-- (UIImage *)imageFromDiskCacheForKey:(NSString *)key {
 
-    // First check the in-memory cache...
-    UIImage *image = [self imageFromMemoryCacheForKey:key];
-    if (image) {
-        return image;
-    }
-
-    // Second check the disk cache...
-    UIImage *diskImage = [self diskImageForKey:key];
-    if (diskImage && self.shouldCacheImagesInMemory) {
-        NSUInteger cost = SDCacheCostForImage(diskImage);
-        [self.memCache setObject:diskImage forKey:key cost:cost];
-    }
-
-    return diskImage;
+- (void)imageFromDiskCacheForKey:(NSString *)key completion:(void(^)(UIImage * image, NSData *imageData))completion{
+    [self imageFromMemoryCacheForKey:key completion:^(UIImage *memoryImage, NSData *memoryImageData){
+        if (memoryImageData)
+        {
+            completion(memoryImage, memoryImageData);
+        }
+        else{
+            [self diskImageForKey:key completion:^(UIImage *diskImage, NSData *diskImageData){
+                if (diskImage && self.shouldCacheImagesInMemory) {
+                    NSUInteger cost = SDCacheCostForImage(diskImage);
+                    [self.memCache setObject:diskImageData forKey:key cost:cost];
+                }
+                if(diskImage){
+                    completion(diskImage, diskImageData);
+                }
+            }];
+        }
+    }];
 }
 
 - (NSData *)diskImageDataBySearchingAllPathsForKey:(NSString *)key {
@@ -332,7 +336,7 @@ FOUNDATION_STATIC_INLINE NSUInteger SDCacheCostForImage(UIImage *image) {
     return nil;
 }
 
-- (UIImage *)diskImageForKey:(NSString *)key {
+- (void)diskImageForKey:(NSString *)key completion:(void(^)(UIImage * diskImage, NSData *diskImageData))completion{
     NSData *data = [self diskImageDataBySearchingAllPathsForKey:key];
     if (data) {
         UIImage *image = [UIImage sd_imageWithData:data];
@@ -340,10 +344,10 @@ FOUNDATION_STATIC_INLINE NSUInteger SDCacheCostForImage(UIImage *image) {
         if (self.shouldDecompressImages) {
             image = [UIImage decodedImageWithImage:image];
         }
-        return image;
+        completion(image, data);
     }
     else {
-        return nil;
+        completion(nil, nil);
     }
 }
 
@@ -357,24 +361,27 @@ FOUNDATION_STATIC_INLINE NSUInteger SDCacheCostForImage(UIImage *image) {
     }
 
     if (!key) {
-        doneBlock(nil, SDImageCacheTypeNone);
+        doneBlock(nil, nil, SDImageCacheTypeNone);
         return nil;
     }
 
-    // First check the in-memory cache...
-    UIImage *image = [self imageFromMemoryCacheForKey:key];
-    if (image) {
-        doneBlock(image, SDImageCacheTypeMemory);
-        return nil;
-    }
+    [self imageFromMemoryCacheForKey:key completion:^(UIImage *memoryImage, NSData *memoryImageData){
+        
+        if (memoryImage) {
+            doneBlock(memoryImage, memoryImageData, SDImageCacheTypeMemory);
+        }
+        else
+        {
+            [self diskImageForKey:key completion:^(UIImage *diskImage, NSData *diskImageData){
+                if (diskImage && self.shouldCacheImagesInMemory) {
+                    NSUInteger cost = SDCacheCostForImage(diskImage);
+                    [self.memCache setObject:diskImageData forKey:key cost:cost];
+                }
+                doneBlock(diskImage, diskImageData, SDImageCacheTypeDisk);
+            }];
+        }
+    }];
 
-    UIImage *diskImage = [self diskImageForKey:key];
-            if (diskImage && self.shouldCacheImagesInMemory) {
-                NSUInteger cost = SDCacheCostForImage(diskImage);
-        [self.memCache setObject:diskImage forKey:key cost:cost];
-    }
-    
-    doneBlock(diskImage, SDImageCacheTypeDisk);
     return nil;
 }
 

SDWebImage/SDWebImageManager.h
@@ -93,7 +93,7 @@ typedef NS_OPTIONS(NSUInteger, SDWebImageOptions) {
 
 typedef void(^SDWebImageCompletionBlock)(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL);
 
-typedef void(^SDWebImageCompletionWithFinishedBlock)(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL);
+typedef void(^SDWebImageCompletionWithFinishedBlock)(UIImage *image, NSData *imageData, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL);
 
 typedef NSString *(^SDWebImageCacheKeyFilterBlock)(NSURL *url);
 

SDWebImage/SDWebImageManager.m
@@ -62,8 +62,14 @@
 
 - (BOOL)cachedImageExistsForURL:(NSURL *)url {
     NSString *key = [self cacheKeyForURL:url];
-    if ([self.imageCache imageFromMemoryCacheForKey:key] != nil) return YES;
-    return [self.imageCache diskImageExistsWithKey:key];
+    __block BOOL isImageAvailable;
+    
+    [self.imageCache imageFromDiskCacheForKey:key completion:^(UIImage *image, NSData *imageData){
+        if (imageData)
+            isImageAvailable = YES;
+    }];
+    
+    return isImageAvailable;
 }
 
 - (BOOL)diskImageExistsForURL:(NSURL *)url {
@@ -75,22 +81,22 @@
                      completion:(SDWebImageCheckCacheCompletionBlock)completionBlock {
     NSString *key = [self cacheKeyForURL:url];
     
-    BOOL isInMemoryCache = ([self.imageCache imageFromMemoryCacheForKey:key] != nil);
-    
-    if (isInMemoryCache) {
-        // making sure we call the completion block on the main queue
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (completionBlock) {
-                completionBlock(YES);
-            }
-        });
-        return;
-    }
-    
-    [self.imageCache diskImageExistsWithKey:key completion:^(BOOL isInDiskCache) {
-        // the completion block of checkDiskCacheForImageWithKey:completion: is always called on the main queue, no need to further dispatch
-        if (completionBlock) {
-            completionBlock(isInDiskCache);
+    [self.imageCache imageFromMemoryCacheForKey:key completion:^(UIImage *image, NSData *imageData){
+        if (imageData){
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (completionBlock) {
+                    completionBlock(YES);
+                }
+            });
+        }
+        
+        else{
+            [self.imageCache diskImageExistsWithKey:key completion:^(BOOL isInDiskCache) {
+                // the completion block of checkDiskCacheForImageWithKey:completion: is always called on the main queue, no need to further dispatch
+                if (completionBlock) {
+                    completionBlock(isInDiskCache);
+                }
+            }];
         }
     }];
 }
@@ -136,7 +142,7 @@
     if (url.absoluteString.length == 0 || (!(options & SDWebImageRetryFailed) && isFailedUrl)) {
         dispatch_main_sync_safe(^{
             NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorFileDoesNotExist userInfo:nil];
-            completedBlock(nil, error, SDImageCacheTypeNone, YES, url);
+            completedBlock(nil, nil, error, SDImageCacheTypeNone, YES, url);
         });
         return operation;
     }
@@ -146,7 +152,7 @@
     }
     NSString *key = [self cacheKeyForURL:url];
 
-    operation.cacheOperation = [self.imageCache queryDiskCacheForKey:key done:^(UIImage *image, SDImageCacheType cacheType) {
+    operation.cacheOperation = [self.imageCache queryDiskCacheForKey:key done:^(UIImage *image, NSData *imageData, SDImageCacheType cacheType) {
         if (operation.isCancelled) {
             @synchronized (self.runningOperations) {
                 [self.runningOperations removeObject:operation];
@@ -160,7 +166,7 @@
                 dispatch_main_sync_safe(^{
                     // If image was found in the cache but SDWebImageRefreshCached is provided, notify about the cached image
                     // AND try to re-download it in order to let a chance to NSURLCache to refresh it from server.
-                    completedBlock(image, nil, cacheType, YES, url);
+                    completedBlock(image, imageData, nil, cacheType, YES, url);
                 });
             }
 
@@ -188,7 +194,7 @@
                 else if (error) {
                     dispatch_main_sync_safe(^{
                         if (!weakOperation.isCancelled) {
-                            completedBlock(nil, error, SDImageCacheTypeNone, finished, url);
+                            completedBlock(nil, nil, error, SDImageCacheTypeNone, finished, url);
                         }
                     });
 
@@ -227,7 +233,7 @@
 
                             dispatch_main_sync_safe(^{
                                 if (!weakOperation.isCancelled) {
-                                    completedBlock(transformedImage, nil, SDImageCacheTypeNone, finished, url);
+                                    completedBlock(transformedImage, data, nil, SDImageCacheTypeNone, finished, url);
                                 }
                             });
                         });
@@ -239,7 +245,7 @@
 
                         dispatch_main_sync_safe(^{
                             if (!weakOperation.isCancelled) {
-                                completedBlock(downloadedImage, nil, SDImageCacheTypeNone, finished, url);
+                                completedBlock(downloadedImage, data, nil, SDImageCacheTypeNone, finished, url);
                             }
                         });
                     }
@@ -262,7 +268,7 @@
         else if (image) {
             dispatch_main_sync_safe(^{
                 if (!weakOperation.isCancelled) {
-                    completedBlock(image, nil, cacheType, YES, url);
+                    completedBlock(image, imageData, nil, cacheType, YES, url);
                 }
             });
             @synchronized (self.runningOperations) {
@@ -273,7 +279,7 @@
             // Image not in cache and download disallowed by delegate
             dispatch_main_sync_safe(^{
                 if (!weakOperation.isCancelled) {
-                    completedBlock(nil, nil, SDImageCacheTypeNone, YES, url);
+                    completedBlock(nil, nil, nil, SDImageCacheTypeNone, YES, url);
                 }
             });
             @synchronized (self.runningOperations) {
@@ -352,7 +358,7 @@
     return [self downloadImageWithURL:url
                               options:options
                              progress:progressBlock
-                            completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+                            completed:^(UIImage *image, NSData *imageData, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
                                 if (completedBlock) {
                                     completedBlock(image, error, cacheType, finished);
                                 }

SDWebImage/SDWebImagePrefetcher.m
@@ -57,7 +57,7 @@
 - (void)startPrefetchingAtIndex:(NSUInteger)index {
     if (index >= self.prefetchURLs.count) return;
     self.requestedCount++;
-    [self.manager downloadImageWithURL:self.prefetchURLs[index] options:self.options progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+    [self.manager downloadImageWithURL:self.prefetchURLs[index] options:self.options progress:nil completed:^(UIImage *image, NSData *imageData, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
         if (!finished) return;
         self.finishedCount++;
 

SDWebImage/UIButton+WebCache.m
@@ -69,7 +69,7 @@ static char imageURLStorageKey;
     self.imageURLStorage[@(state)] = url;
 
     __weak __typeof(self)wself = self;
-    id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+    id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:nil completed:^(UIImage *image, NSData *imageData, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
         if (!wself) return;
         dispatch_main_sync_safe(^{
             __strong UIButton *sself = wself;
@@ -117,7 +117,7 @@ static char imageURLStorageKey;
 
     if (url) {
         __weak __typeof(self)wself = self;
-        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:nil completed:^(UIImage *image, NSData *imageData, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
             if (!wself) return;
             dispatch_main_sync_safe(^{
                 __strong UIButton *sself = wself;

SDWebImage/UIImage+MultiFormat.m
@@ -23,12 +23,11 @@
     }
     
     UIImage *image;
-    NSString *imageContentType = [NSData sd_contentTypeForImageData:data];
-    if ([imageContentType isEqualToString:@"image/gif"]) {
+    if ([self isAnimatedGifImageData:data]) {
         image = [UIImage sd_animatedGIFWithData:data];
     }
 #ifdef SD_WEBP
-    else if ([imageContentType isEqualToString:@"image/webp"])
+    else if ([[NSData sd_contentTypeForImageData:data] isEqualToString:@"image/webp"])
     {
         image = [UIImage sd_imageWithWebPData:data];
     }
@@ -113,6 +112,11 @@
     return orientation;
 }
 
++(BOOL)isAnimatedGifImageData:(NSData *)imageData
+{
+    NSString *imageContentType = [NSData sd_contentTypeForImageData:imageData];
+    return [imageContentType isEqualToString:@"image/gif"];
+}
 
 
 @end

SDWebImage/UIImageView+HighlightedWebCache.m
@@ -34,7 +34,7 @@
 
     if (url) {
         __weak __typeof(self)wself = self;
-        id<SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:progressBlock completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+        id<SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:progressBlock completed:^(UIImage *image, NSData *imageData,NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
             if (!wself) return;
             dispatch_main_sync_safe (^
                                      {

SDWebImage/UIImageView+WebCache.m
@@ -59,7 +59,7 @@ static char TAG_ACTIVITY_SHOW;
         }
 
         __weak __typeof(self)wself = self;
-        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:progressBlock completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:progressBlock completed:^(UIImage *image, NSData *imageData, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
             [wself removeActivityIndicator];
             if (!wself) return;
             dispatch_main_sync_safe(^{
@@ -97,9 +97,11 @@ static char TAG_ACTIVITY_SHOW;
 
 - (void)sd_setImageWithPreviousCachedImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options progress:(SDWebImageDownloaderProgressBlock)progressBlock completed:(SDWebImageCompletionBlock)completedBlock {
     NSString *key = [[SDWebImageManager sharedManager] cacheKeyForURL:url];
-    UIImage *lastPreviousCachedImage = [[SDImageCache sharedImageCache] imageFromDiskCacheForKey:key];
     
-    [self sd_setImageWithURL:url placeholderImage:lastPreviousCachedImage ?: placeholder options:options progress:progressBlock completed:completedBlock];    
+    [[SDImageCache sharedImageCache] imageFromDiskCacheForKey:key completion:^(UIImage *image, NSData *imageData){
+        [self sd_setImageWithURL:url placeholderImage:image ?: placeholder options:options progress:progressBlock completed:completedBlock];
+    }];
+
 }
 
 - (NSURL *)sd_imageURL {
@@ -113,7 +115,7 @@ static char TAG_ACTIVITY_SHOW;
     NSMutableArray *operationsArray = [[NSMutableArray alloc] init];
 
     for (NSURL *logoImageURL in arrayOfURLs) {
-        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:logoImageURL options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:logoImageURL options:0 progress:nil completed:^(UIImage *image, NSData *imageData, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
             if (!wself) return;
             dispatch_main_sync_safe(^{
                 __strong UIImageView *sself = wself;